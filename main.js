98a726d0d83b600d25f6c10cd6c77cbde92d0d13
Bruno Matias <69431006+brunojosematias@users.noreply.github.com>
2022-08-10 20:41:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ const menu = document.querySelector('.menu .toggle .action');
 menu.addEventListener('click', () => {
   toggle.classList.toggle('show');
 })
+
+
+const elements = document.querySelectorAll('.menu ul li');
+
+elements.forEach(element => {
+
+  element.addEventListener('click', () => {
+    let elements = document.querySelectorAll('.menu ul li');
+
+    for (const option of elements) {
+     option.classList.remove('select')
+    }
+
+    if (element.classList != 'select') {
+      element.classList.add('select')
+    }
+  })
+})
